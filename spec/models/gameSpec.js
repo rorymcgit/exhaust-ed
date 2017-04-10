c06da7cd2f0b7efea1aef5215574fefbb1ed9666
joemaidman <joemaidman@gmail.com>
2017-04-10 17:28:24 +0100

    Adds game controller, adds getDuration to game model

---
+++ spec/models/gameSpec.js
@@ -0,0 +1,48 @@
+var game = new Game();
+
+describe("Game", function() {
+  it("exists", function() {
+    expect(game).toBeDefined();
+  });
+
+  it("has a start time", function() {
+    expect(game.startTime).toBeDefined();
+  });
+
+  it("has an end time", function() {
+    expect(game.endTime).toBeDefined();
+  });
+
+  it("starts", function() {
+    game.begin();
+    expect(game._isPlaying()).toEqual(true);
+  });
+
+  it("ends", function() {
+    game.begin();
+    game.end();
+    expect(game._isPlaying()).toEqual(false);
+  });
+
+  it("sets the start time", function(){
+    game.setStartTime();
+    expect(game.startTime).not.toBe(null)
+  });
+
+  it("sets the end time", function(){
+    game.setStartTime();
+    game.setEndTime();
+    expect(game.endTime).not.toBe(null)
+  });
+
+  it("calculates the duration of the game", function(){
+    var dummyStartDate = new Date(2017,4,5,10,0,0);
+    var dummyEndDate = new Date(2017,4,5,10,0,5);
+    var spy = spyOn(window, 'Date').and.returnValue(dummyStartDate)
+    game.setStartTime();
+    spy.and.returnValue(dummyEndDate)
+    game.setEndTime();
+    expect(game.getDuration()).toEqual(5000)
+  })
+
+});
---
+++ src/models/game.js
@@ -0,0 +1,35 @@
+(function(exports) {
+
+  function Game() {
+    this.playing = false;
+    this.startTime = null;
+    this.endTime = null;
+    game = this;
+  }
+
+  Game.prototype.begin = function() {
+    this.playing = true;
+  };
+
+  Game.prototype.end = function() {
+    this.playing = false;
+  };
+
+  Game.prototype.setStartTime = function () {
+    this.startTime = new Date();
+  };
+
+  Game.prototype.setEndTime = function () {
+    this.endTime = new Date();
+  };
+
+  Game.prototype.getDuration = function () {
+    return this.endTime - this.startTime;
+  };
+
+  Game.prototype._isPlaying = function() {
+    return this.playing;
+  };
+
+  exports.Game = Game;
+})(this);
