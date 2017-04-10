2ba5914d34f73e66962b6cc046efa29dff645e05
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-04-10 16:03:44 +0100

    Start and end a game

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,7 +1,18 @@
 var game = new Game();
 
-describe("Game", function(){
-  it("exists", function(){
+describe("Game", function() {
+  it("exists", function() {
     expect(game).toBeDefined();
+  });
+
+  it("starts", function() {
+    game.begin();
+    expect(game._isPlaying()).toEqual(true);
+  });
+
+  it("ends", function() {
+    game.begin();
+    game.end();
+    expect(game._isPlaying()).toEqual(false);
   });
 });
--- src/game.js
+++ src/game.js
@@ -1,7 +1,20 @@
 (function(exports) {
 
   function Game() {
+    this.playing = false;
   }
+
+  Game.prototype.begin = function() {
+    this.playing = true;
+  };
+
+  Game.prototype.end = function() {
+    this.playing = false;
+  };
+
+  Game.prototype._isPlaying = function() {
+    return this.playing;
+  };
 
   exports.Game = Game;
 })(this);
