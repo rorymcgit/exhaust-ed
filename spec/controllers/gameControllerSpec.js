477366b8ce11bd14a639ca84ee9dc61bda98a0a7
Christos <chris.paraskeva10@gmail.com>
2017-04-10 19:03:37 +0100

    failing test when testing bindKey method / need to get spyOn working

--- spec/controllers/gameControllerSpec.js
+++ spec/controllers/gameControllerSpec.js
@@ -1,8 +1,14 @@
-var game = new Game();
+var gameController = new GameController();
+
 
 describe("GameController", function() {
-  it("starts a game", function() {
+  // it("starts a game", function() {
+  //
+  // });
 
+  it("registers a 'keyup' event listener", function() {
+    spyOn(gameController, 'bindKeys').and.callThrough();
+    gameController.bindKeys();
+    expect(window.addEventListener).toHaveBeenCalledWith("keyup", keyPressed, false)
   });
-
 });
--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -2,8 +2,25 @@
 
   function GameController(game = new Game()) {
     this.game = game;
+  }
+
+GameController.prototype.draw = function () {
+
+};
+
+GameController.prototype.keyPressed = function () {
+
+};
+
+GameController.prototype.bindKeys = function () {
+  // console.log(this.keyPressed)
+  window.addEventListener("keyup", keyPressed, false);
+};
 
 
+// GameController.prototype.getCanvas = function () {
+//
+// };
 
   exports.GameController = GameController;
 })(this);
